c42ebf40d3e8632a8cf8d1105c01534bd6c7d59a
PhilLar <phillarov@gmail.com>
2019-09-09 14:48:32 +0300

    db added

--- cmd/images-back/main.go
+++ cmd/images-back/main.go
@@ -10,29 +10,66 @@ import (
 	"os/signal"
 	"strconv"
 	"time"
+	_ "github.com/lib/pq"
+	"database/sql"
 )
 
 var port int
+var db string
 
 func init() {
-	defPort := 5000
+	defPort := 8080
+	var defDB string
 	if portVar, ok := os.LookupEnv("PORT"); ok {
 		if portValue, err := strconv.Atoi(portVar); err == nil {
 			defPort = portValue
 		}
 	}
+	if dbVar, ok := os.LookupEnv("DATABASE_URL"); ok {
+		defDB = dbVar
+	}
 	flag.IntVar(&port, "port", defPort, "port to listen on")
+	flag.StringVar(&db, "db", defDB, "database to connect to")
 }
 
 func main() {
 	flag.Parse()
 	e := echo.New()
-	e.Static("/static", "static")
 	e.Logger.SetLevel(log.INFO)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.File("/index", "static/index.html")
+
+	e.GET("dbTest", func(c echo.Context) error {
+		//connStr := "user=images password=images_go dbname=imagesapp sslmode=disable"
+		url := db
+		dbPsql, err := sql.Open("postgres", url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer dbPsql.Close()
+
+		err = dbPsql.Ping()
+		if err != nil {
+			panic(err)
+		}
+		log.Print("DB OK!")
+		_, err = dbPsql.Exec("CREATE TABLE IF NOT EXISTS images_table3 (" +
+			"id serial PRIMARY KEY," +
+			"size integer NOT NULL," +
+			"log VARCHAR (50) NOT NULL" +
+			")")
+		if err != nil {
+			panic(err)
+		}
+		return c.String(http.StatusOK, "DB_TABLE images CREATED!")
+	})
+
+	e.Static("/", "static")
+	e.Static("/files", "files")
+	// the line below loads only one file - bad
+	//e.File("/index", "static/index.html")
 
 	go func() {
 		if err := e.Start(":" + strconv.Itoa(port)); err != nil {
@@ -50,4 +87,6 @@ func main() {
 	if err := e.Shutdown(ctx); err != nil {
 		e.Logger.Fatal(err)
 	}
+
+
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.2.0
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
 )
